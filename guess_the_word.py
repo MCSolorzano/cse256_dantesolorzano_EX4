5f5904bee3533d9417795a2daf4480bbabe7a3fe
Dante <DAN2253214@maricopa.edu>
2024-11-13 22:42:09 -0700

    implement first half
    has some issues with detecting if a guessed letter was valid or not, but will fix.

--- guess_the_word.py
+++ guess_the_word.py
@@ -2,20 +2,38 @@ import random
 
 words = ["Apple", "Peach", "Blueberry", "Strawberry", "Mango", "Durian", "Tomato"]
 guessed_letters = []
-word = '' # start blank
-available_attempts = 5
+word = random.choice(words).lower() #ensures that guesses are not case sensitive
+available_attempts = 5 
 attempts = 0
 
 def build_word():
     built_word= ''
     return built_word.join(x if x in guessed_letters else '_' for x in word)
 
+
 def start():
+    global attempts
+    print("Start!")
     while True:
-        global word
-        print("Start!")
-        word = random.choice(words).lower() #ensures that guesses are not case sensitive
-        print(build_word())
+        if (available_attempts != attempts):
+            built_word = build_word()
+            print(built_word)
+            if ("_" in built_word):
+                print("Guesses remaining: " + str(available_attempts - attempts))
+                user_letter = input("Make a guess! Please only insert a letter: ").lower()
+                if (len(user_letter) == 1):
+                    attempts += 1
+                    guessed_letters.append(user_letter)
+                    
+                    print('wa')
+                else:
+                    print('Please only insert a letter!')
+            else:
+                print("You win!")
+        else:
+            print("You lost!")
+            break
+
 
 if __name__ == "__main__":
     start()
